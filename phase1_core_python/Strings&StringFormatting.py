dbbc32cc59925d485c0aadee2a5f516d3357a658
hzeeshandev <hzeeshanhargan786@gmail.comm>
2026-07-11 09:45:13 +0500

    Phase 1 Completed✅

---
+++ phase1_core_python/Strings&StringFormatting.py
@@ -0,0 +1,39 @@
+# Creating strings
+
+name = "Zeeshan"
+message = 'this is python tutorial'
+multiline = """this is multiline
+comment"""
+print(name)
+print(message)
+print(multiline)
+
+# Indexing & Slicing
+
+print(name[0])
+print(name[3])
+print(name[-1])
+print(name[0:3])
+print(name[:3])
+print(name[3:])
+print(name[::-1])
+
+# Common String Methods
+
+s = "  Hello World  "
+print(s.strip())
+print(s.split())
+print(s.lower())
+print(s.upper())
+print(len(s))
+print(s.startswith("  Hel"))
+print(s.replace("World", "Zeeshan"))
+print("-".join(["a","b","c"]))
+
+#f-strings (the modern, correct way to format strings)
+
+stud = "Zeesahn"
+gpa = 2.5
+age = 22
+print(f"my name is {stud} and im {age} year old and my gpa is {gpa:.1f}.")
+print(f"{stud.upper()} is a student.")
